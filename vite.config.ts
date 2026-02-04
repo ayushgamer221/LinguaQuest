6c73aadc516056dfc0facef53a10333a3bf0551a
ayushgamer221 <162696917+ayushgamer221@users.noreply.github.com>
2026-02-04 15:58:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: "client",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "client"),
+    },
+  },
 });
